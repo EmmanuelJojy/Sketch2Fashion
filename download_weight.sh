d9318c761473e8cbe472dc247d49821ed9a29fe5
Emmanuel Jojy <emmijojy@gmail.com>
2023-05-09 14:52:07 +0000

    Add unet and pix2pix

---
+++ download_weight.sh
@@ -0,0 +1,6 @@
+mkdir models
+cd models
+wget -q --show-progress https://github.com/EmmanuelJojy/Sketch2Fashion/releases/download/v1.0.0/cyclegan-resnet.pth
+wget -q --show-progress https://github.com/EmmanuelJojy/Sketch2Fashion/releases/download/v1.0.0/cyclegan-unet.pth
+wget -q --show-progress https://github.com/EmmanuelJojy/Sketch2Fashion/releases/download/v1.0.0/pix2pix-color.pth
+wget -q --show-progress https://github.com/EmmanuelJojy/Sketch2Fashion/releases/download/v1.0.0/pix2pix.pth
--- transform.py
+++ transform.py
@@ -2,40 +2,43 @@ from collections import OrderedDict
 import torch
 from PIL import Image
 import torchvision.transforms as transforms
-from networks import define_G
 
-# Define the path to your pre-trained model
-model_path = "model.pth"
+from lib.networks import define_G
+from lib.util import tensor2im, save_image
 
-# Define the device to use for computations
-# device = torch.device('cpu')
+models = {
+    'resnet': {'path': 'models/cyclegan-resnet.pth', 'input_nc': 3, 'output_nc': 3, 'netG': "resnet_9blocks", 'norm': "instance", },
+    'unet': {'path': 'models/cyclegan-unet.pth', 'input_nc': 3, 'output_nc': 3, 'netG': "unet_256", 'norm': "instance", },
+    'pix2pix': {'path': 'models/pix2pix.pth', 'input_nc': 3, 'output_nc': 3, 'netG': "unet_256", 'norm': "batch", },
+    'color': {'path': 'models/pix2pix-color.pth', 'input_nc': 1, 'output_nc': 2, 'netG': "unet_256", 'norm': "instance", },
+}
 
-# Load the pre-trained model
-model_dict = torch.load(model_path)
-new_dict = OrderedDict()
-for k, v in model_dict.items():
-    # load_state_dict expects keys with prefix 'module.'
-    new_dict[k] = v
-
-# make sure you pass the correct parameters to the define_G method
-generator_model = define_G(input_nc=3, output_nc=3, ngf=64, netG="resnet_9blocks",
-                           norm="instance", use_dropout=False, init_gain=0.02, gpu_ids=[])
-generator_model.load_state_dict(new_dict)
-
-# Set the model to evaluation mode
-generator_model.eval()
-
-# Define a data transformation pipeline for your input image
 transform = transforms.Compose([
     transforms.Resize((256, 256)),
     transforms.Grayscale(num_output_channels=3),
     transforms.ToTensor(),
-    transforms.Normalize((0.5,), (0.5,))
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
 
 
-def sketch2fashion(input_image_path, output_image_path):
-    input_image = Image.open(input_image_path)
+def create_generator(model):
+    model_dict = torch.load(model['path'])
+    new_dict = OrderedDict()
+    for k, v in model_dict.items():
+        new_dict[k] = v
+    generator_model = define_G(input_nc=model['input_nc'], output_nc=model['output_nc'], netG=model['netG'],
+                               norm=model['norm'], ngf=64, use_dropout=False, init_gain=0.02, gpu_ids=[])
+    generator_model.load_state_dict(new_dict)
+
+    # Set the model to evaluation mode
+    generator_model.eval()
+
+    return generator_model
+
+
+def sketch2fashion(generator_model, input_image_path='test.jpg', output_image_path='out.jpg'):
+    input_image = Image.open(input_image_path).convert('RGB')
+    image_size = input_image.size
 
     # Preprocess the input image
     input_tensor = transform(input_image).unsqueeze(0)
@@ -45,11 +48,24 @@ def sketch2fashion(input_image_path, output_image_path):
         output_tensor = generator_model(input_tensor)
 
     # Postprocess the output image
-    output_image = transforms.functional.to_pil_image(
-        output_tensor.squeeze().cpu())
-    output_image.save(output_image_path)
+    output_image = tensor2im(output_tensor)
+    save_image(output_image, output_image_path, image_size)
 
 
-src = "testA.png"
-tar = "testB.png"
-sketch2fashion(src, tar)
+# set up generators
+resnet = create_generator(models['resnet'])
+print("CycleGAN (Gen: Resnet): Ready", end='\n\n')
+
+unet = create_generator(models['unet'])
+print("CycleGAN (Gen: U -NET): Ready", end='\n\n')
+
+pix2pix = create_generator(models['pix2pix'])
+print("Pix 2 Pix (Gen: U NET): Ready", end='\n\n')
+
+# color = create_generator(models['color'])
+# print("Pix 2 Pix (Var: Color): Ready", end='\n\n')
+
+
+sketch2fashion(resnet, input_image_path='test.jpg', output_image_path='resnet.jpg')
+sketch2fashion(resnet, input_image_path='test.jpg', output_image_path='unet.jpg')
+sketch2fashion(resnet, input_image_path='test.jpg', output_image_path='pix2pix.jpg')
