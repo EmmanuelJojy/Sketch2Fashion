84f4d1150fcb83792c355684dbc7389c08f14ad7
Emmanuel Jojy <emmijojy@gmail.com>
2023-05-14 17:36:22 +0000

    Add cycle unet

--- transform.py
+++ transform.py
@@ -8,9 +8,10 @@ from lib.networks import define_G
 from lib.util import tensor2im, save_image
 
 models = {
-    'resnet': {'path': 'models/cyclegan-resnet.pth', 'input_nc': 3, 'output_nc': 3, 'netG': "resnet_9blocks", 'norm': "instance", },
-    'unet': {'path': 'models/cyclegan-unet.pth', 'input_nc': 3, 'output_nc': 3, 'netG': "unet_256", 'norm': "instance", },
-    'pix2pix': {'path': 'models/pix2pix.pth', 'input_nc': 3, 'output_nc': 3, 'netG': "unet_256", 'norm': "batch", },
+    'resnet': {'path': 'models/cyclegan-resnet.pth', 'netG': "resnet_9blocks", 'norm': "instance", },
+    'unet': {'path': 'models/cyclegan-unet.pth', 'netG': "unet_256", 'norm': "instance", },
+    'unet_new': {'path': 'models/cyclegan-unet-new.pth', 'netG': "unet_256", 'norm': "instance", },
+    'pix2pix': {'path': 'models/pix2pix.pth', 'netG': "unet_256", 'norm': "batch", },
 }
 
 transform = transforms.Compose([
@@ -20,14 +21,20 @@ transform = transforms.Compose([
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
 
+transform_without_grayscale = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
 
-def create_generator(model):
+
+def create_generator(model, ngf=64):
     model_dict = torch.load(model['path'])
     new_dict = OrderedDict()
     for k, v in model_dict.items():
         new_dict[k] = v
-    generator_model = define_G(input_nc=model['input_nc'], output_nc=model['output_nc'], netG=model['netG'],
-                               norm=model['norm'], ngf=64, use_dropout=False, init_gain=0.02, gpu_ids=[])
+    generator_model = define_G(input_nc=3, output_nc=3, netG=model['netG'],
+                               norm=model['norm'], ngf=ngf, use_dropout=False, init_gain=0.02, gpu_ids=[])
     generator_model.load_state_dict(new_dict)
 
     # Set the model to evaluation mode
@@ -36,12 +43,15 @@ def create_generator(model):
     return generator_model
 
 
-def sketch2fashion(generator_model, input_image_path='test.jpg', output_image_path='out.jpg'):
+def sketch2fashion(generator_model, input_image_path='test.jpg', output_image_path='out.jpg', grayscale=True):
     input_image = Image.open(input_image_path).convert('RGB')
     image_size = input_image.size
 
     # Preprocess the input image
-    input_tensor = transform(input_image).unsqueeze(0)
+    if grayscale:
+        input_tensor = transform(input_image).unsqueeze(0)
+    else:
+        input_tensor = transform_without_grayscale(input_image).unsqueeze(0)
 
     # Pass the input image through the generator model
     with torch.no_grad():
@@ -52,22 +62,22 @@ def sketch2fashion(generator_model, input_image_path='test.jpg', output_image_pa
     save_image(output_image, output_image_path, image_size)
 
 
-# set up generators
+# # set up generators
 resnet = create_generator(models['resnet'])
 print("CycleGAN (Gen: Resnet): Ready", end='\n\n')
 
 unet = create_generator(models['unet'])
 print("CycleGAN (Gen: U -NET): Ready", end='\n\n')
 
-pix2pix = create_generator(models['pix2pix'])
-print("Pix 2 Pix (Gen: U NET): Ready", end='\n\n')
+unet_cycle = create_generator(models['unet_new'], ngf=128)
+print("CycleGAN (Gen: UNETx2): Ready", end='\n\n')
 
 
 # RUNNER
 
 sketch2fashion(resnet, input_image_path='test.jpg', output_image_path='out_resnet.jpg')
 sketch2fashion(unet, input_image_path='test.jpg', output_image_path='out_unet.jpg')
-sketch2fashion(pix2pix, input_image_path='test.jpg', output_image_path='out_pix2pix.jpg')
+sketch2fashion(unet_cycle, input_image_path='test.jpg', output_image_path='out_unet_cycle.jpg')
 
 # import time
 
